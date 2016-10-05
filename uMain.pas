2b5ab36367162f31c9682233af3860bf3c6678cd
rt-inconnect <r.toshpulatov@gmail.com>
2016-10-06 01:33:53 +0500

    add report

--- uMain.pas
+++ uMain.pas
@@ -4,15 +4,20 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, ShellAPI, Vcl.ExtCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, ShellAPI, Vcl.ExtCtrls,
+  frxClass, frxExportPDF;
 
 type
   TfmMain = class(TForm)
     OpenDialog: TOpenDialog;
-    Memo: TMemo;
     pHeader: TPanel;
     Button: TButton;
+    frxPDFExport: TfrxPDFExport;
+    frxReport: TfrxReport;
+    Button1: TButton;
     procedure ButtonClick(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+
   private
     { Private declarations }
   public
@@ -25,6 +30,42 @@ var
 implementation
 
 {$R *.dfm}
+
+function parse(str, str2, ob: string): string;
+var
+len: integer;
+begin
+  len := length(str);
+  result := Copy(ob, pos(str, ob) + len - 1, pos(str2, ob) - pos(str, ob) - len);
+end;
+
+function parseToEnd(str, ob: string): string;
+var
+len: integer;
+begin
+  len := length(str);
+  result := Copy(ob, pos(str, ob), length(ob));
+end;
+
+function getFirstDigit(str: string): string;
+var
+ i : Smallint;
+begin
+ for i:=1 to Length(str) do if (str[i] in ['0'..'9']) then begin result := str[i]; break; end;
+end;
+
+procedure TfmMain.Button1Click(Sender: TObject);
+var
+  vList: TStringList;
+begin
+  vList := TStringList.Create;
+  vList.LoadFromFile('out.txt');
+  ShowMessage(getFirstDigit(vList[33]));
+  TfrxMemoView(frxReport.FindObject('pkName')).Memo.Text := parse('', ' ' + getFirstDigit(vList[33]), vList[33]);
+  TfrxMemoView(frxReport.FindObject('pkAddress')).Memo.Text := parse(' ' + getFirstDigit(vList[33]), 'Phone:', vList[33]);
+  TfrxMemoView(frxReport.FindObject('pkPhone')).Memo.Text := parseToEnd('Phone:', vList[33]);
+  frxReport.ShowReport(true);
+end;
 
 procedure TfmMain.ButtonClick(Sender: TObject);
 var filename : String;
@@ -39,7 +80,6 @@ begin
     wExe := '"' + ExtractFilePath(Application.ExeName) + 'pdftotext.exe"';
     pExe := PWideChar(wExe);
     ShellExecute(0, 'open', pExe, pPath, nil, SW_HIDE);
-    Memo.Lines.LoadFromFile('out.txt');
   end;
 end;
 
